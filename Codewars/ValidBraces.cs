ff18d82cd986c9b86fb4a6a91339d4ea03b7afeb
Ulf Kühnle <ulf.kuehnle@interone.de>
2024-04-11 15:18:34 +0200

    added kata solution ValidBraces

---
+++ Codewars/ValidBraces.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars;
+
+public class ValidBraces
+{
+    // https://www.codewars.com/kata/5277c8a221e209d3f6000b56/csharp
+    public static bool Validate(string input)
+    {
+        var dict = new Dictionary<char, char> { { ')', '(' }, { ']', '[' }, { '}', '{' } };
+        var stack = new Stack<char>();
+        foreach (var c in input)
+        {
+            if (dict.ContainsValue(c))
+            {
+                stack.Push(c);
+            }
+            else if (dict.ContainsKey(c) && stack.Any() && stack.Peek() == dict[c])
+            {
+                stack.Pop();
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        return !stack.Any();
+    }
+}
---
+++ CodewarsTest/ValidBracesTests.cs
@@ -0,0 +1,20 @@
+﻿using Codewars;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodewarsTest;
+
+[TestClass]
+public class ValidBracesTests
+{
+    [DataTestMethod]
+    [DataRow("(){}[]",true)]
+    [DataRow("([{}])",true)]
+    [DataRow("(}",false)]
+    [DataRow("[(])",false)]
+    [DataRow("[({})](]",false)]
+    public void ValidBracesTest(string input, bool result)
+    {
+        ValidBraces.Validate(input).Should().Be(result);
+    }
+}
