c0f5bbe8af869105256565484c1326bf4d392273
Ulf Kühnle <ulf.kuehnle@interone.de>
2024-10-10 17:22:59 +0200

    kata BreadcrumbGenerator

---
+++ Codewars/BreadcrumbGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars;
+
+// https://www.codewars.com/kata/563fbac924106b8bf7000046/csharp
+public class BreadcrumbGenerator
+{
+    public static string GenerateBC(string url, string separator)
+    {
+        var uri = new Uri(ToValidUrl(url));
+        var parts = uri.Segments.Select(x => x.Replace("/","")).ToArray();
+        
+        var lastPart = parts.Last();
+        lastPart = RemoveParts(lastPart, new[]{".html",".htm",".php",".asp"});
+        if (lastPart == "index")
+        {
+            parts = parts.Take(parts.Length - 1).ToArray();
+        }
+        else
+        {
+            parts[^1] = lastPart;
+        }
+
+        if (parts.Length == 1) return GetSpan("HOME");
+        
+        var bcParts = new List<string> { GetLink("/", "HOME") };
+        var link = "/";
+        for (var idx = 1; idx < parts.Length - 1; idx++)
+        {
+            link += parts[idx] + "/";
+            bcParts.Add(GetLink(link, parts[idx]));
+        }
+        bcParts.Add(GetSpan(parts.Last()));
+        return string.Join(separator, bcParts);
+    }
+    
+    private static string ToValidUrl(string url) =>  !url.StartsWith("http") ? $"https://{url}" : url;
+
+    private static string RemoveParts(string text, string[] removeParts)
+    {
+        return removeParts.Aggregate(text, (current, removal) => current.Replace(removal, ""));
+    }
+
+    private static string GetLink(string path, string text) => $"<a href=\"{path}\">{ToOutputText(text)}</a>";
+    
+    private static string GetSpan(string text) => $"<span class=\"active\">{ToOutputText(text)}</span>";
+
+    private static string ToOutputText(string text)
+    {
+        var result = text.Replace('-',' ');
+        if (text.Length > 30)
+        {
+            var parts = text.Split('-');
+            result = new string(parts.Where(w => !ReservedWords.Contains(w)).Select(x => x[0]).ToArray());
+        }
+        return result.ToUpper();
+    }
+
+    private static readonly string[] ReservedWords = {"the", "of", "in", "from", "by", "with", "and", "or", "for", "to", "at", "a"};
+}
---
+++ CodewarsTest/BreadcrumbGeneratorTests.cs
@@ -0,0 +1,24 @@
+﻿using Codewars;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodewarsTest;
+
+[TestClass]
+public class BreadcrumbGeneratorTests
+{
+    [DataTestMethod]
+    [DataRow("mysite.com/pictures/holidays.html", " : ", "<a href=\"/\">HOME</a> : <a href=\"/pictures/\">PICTURES</a> : <span class=\"active\">HOLIDAYS</span>")]
+    [DataRow("www.codewars.com/users/GiacomoSorbi", " / ", "<a href=\"/\">HOME</a> / <a href=\"/users/\">USERS</a> / <span class=\"active\">GIACOMOSORBI</span>")]
+    [DataRow("www.microsoft.com/docs/index.htm", " * ", "<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>")]
+    [DataRow("www.microsoft.com/docs/index.htm?key=value", " * ", "<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>")]
+    [DataRow("www.microsoft.com/docs/index.htm#anchor", " * ", "<a href=\"/\">HOME</a> * <span class=\"active\">DOCS</span>")]
+    [DataRow("www.microsoft.com", " * ", "<span class=\"active\">HOME</span>")]
+    [DataRow("mysite.com/very-long-url-to-make-a-silly-yet-meaningful-example/example.asp"," > ","<a href=\"/\">HOME</a> > <a href=\"/very-long-url-to-make-a-silly-yet-meaningful-example/\">VLUMSYME</a> > <span class=\"active\">EXAMPLE</span>")]
+    [DataRow("https://www.agcpartners.co.uk/index.html", " >>> ", "<span class=\"active\">HOME</span>")]
+    [DataRow("https://www.agcpartners.co.uk/", " >>> ", "<span class=\"active\">HOME</span>")]
+    public void BasicTests(string url, string separator, string expected)
+    {
+        BreadcrumbGenerator.GenerateBC(url, separator).Should().Be(expected);
+    }
+}
