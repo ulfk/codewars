690c1486dde0053708e993796314ba37f4ca4e8f
Ulf Kühnle <ulf.kuehnle@interone.de>
2024-10-10 08:51:18 +0200

    optimized the code

--- Codewars/JosephusSurvivor.cs
+++ Codewars/JosephusSurvivor.cs
@@ -3,20 +3,18 @@
 namespace Codewars;
 
 // https://www.codewars.com/kata/555624b601231dc7a400017a/csharp
-public class JosephusSurvivor
+public static class JosephusSurvivor
 {
     public static int JosSurvivor(int n, int k)
     {
         // cpu/ram intensive solution...
         
         var values = Enumerable.Range(1, n).ToArray();
-        var idx = -1;
+        var idx = 0;
         while (values.Length > 1)
         {
-            idx += k;
-            while(idx >= values.Length) idx -= values.Length;
+            idx = (idx + (k - 1)) % values.Length; 
             values = values.Where(v => v != values[idx]).ToArray();
-            idx--;
         }
         return values[0];
     }
